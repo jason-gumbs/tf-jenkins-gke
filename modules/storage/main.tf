781fcd21da55b2580d4c5faa3927cf282fdb8182
Baruch Banks <baruch.banks@cod-it.tech>
2021-11-22 13:45:13 -0500

    updating terraform config

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -2,3 +2,15 @@ resource "google_storage_bucket" "codit-test-buckets" {
   name     = var.bucket_name
   location = "US"
 }
+
+data "google_iam_policy" "admin" {
+  binding {
+    role = var.georgia
+    members = var.washington
+  }
+}
+
+resource "google_storage_bucket_iam_policy" "policy" {
+  bucket = var.new_york
+  policy_data = data.google_iam_policy.admin.policy_data
+}
--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -1,1 +1,13 @@
 variable "bucket_name" {}
+
+variable "new_york" {
+  type = string
+}
+
+variable "georgia" {
+  type = string
+}
+
+variable "washington" {
+  type = list(string)
+}
