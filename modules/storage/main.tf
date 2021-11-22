0e6c1b23b4777a2be122f776b9a200845e3e0759
Baruch Banks <baruch.banks@cod-it.tech>
2021-11-22 11:36:31 -0500

    second try

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -2,3 +2,10 @@ resource "google_storage_bucket" "codit-test-buckets" {
   name     = var.bucket_name
   location = "US"
 }
+
+resource "google_storage_bucket_iam_binding" "binding" {
+  bucket = var.baruch_bucket
+  role = var.engineer
+  members = var.christmaslist
+
+}
